fb81d4c404b5bc8205944b442b2fcce26dba6334
Jared Grippe <github.com@deadlyicon.com>
2009-01-27 23:50:54 -0800

    started building out aboutme page including adding foaf

--- app/controllers/aboutme_controller.rb
+++ app/controllers/aboutme_controller.rb
@@ -1,2 +1,12 @@
 class AboutmeController < ApplicationController
+  
+  def index
+  end
+  
+  def foaf
+    respond_to do |format|
+      format.rdf
+    end
+  end
+  
 end
--- app/helpers/aboutme_helper.rb
+++ app/helpers/aboutme_helper.rb
@@ -1,2 +1,21 @@
 module AboutmeHelper
+  
+  def stats(html_options={}, &block)
+    content_tag(:table, {:id => 'stats'}.merge(html_options) ) do
+      content_tag(:tbody, capture(&block))
+    end
+  end
+  
+  def stat( description, value=nil, &block)
+    value = block_given? ? capture(&block) : value
+    content_tag( :tr, :class => "stat #{description.to_s.underscore}" )do
+      content_tag( :td, description.to_s.humanize, :class => 'description' ) + \
+      content_tag( :td, value.to_s, :class => 'value' )
+    end
+  end
+  
+  def icon( name, url, options={} )
+    link_to image_tag("aboutme/icons/#{name}"), url, :class => 'icon'
+  end
+  
 end
