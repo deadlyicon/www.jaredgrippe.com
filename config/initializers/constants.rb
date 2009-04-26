2903e2399bfdc99390f0f110ad39e362f10be1cc
Jared Grippe <jaredgrippe@jared-grippes-macbook-pro.local>
2009-04-26 16:08:08 -0700

    feed burned posts feed and cleaned up constants into an initializer

---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+BLOG_POSTS_FEED_URL = 'http://feeds2.feedburner.com/jaredgrippecom'
+GOOGLE_PROFILE_URL  = 'http://www.google.com/s2/profiles/104443186735122889135'
+TWITTER_URL         = 'http://www.twitter.com/jaredgrippe'
+FACEBOOK_URL        = 'http://www.facebook.com/profile.php?id=509634696'
+DEADLYICON_BLOG_URL = 'http://blog.deadlyicon.com/'
+FLICKR_URL          = 'http://www.flickr.com/photos/deadlyicon/'
+DELICIOUS_URL       = 'http://del.icio.us/deadlyicon'
+LINKEDIN_URL        = 'http://www.linkedin.com/in/deadlyicon'
